e83931659902863828c5766b04daa2172fa7c88e
HossainNur <nurhossainsani1@gmail.com>
2024-07-15 16:04:51 +0600

    calculate bmi score pass bmi score to ResultFragment

--- app/src/main/java/com/nurhossain/bmicalculator/HomeFragment.kt
+++ app/src/main/java/com/nurhossain/bmicalculator/HomeFragment.kt
@@ -7,6 +7,9 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
+import androidx.core.os.bundleOf
+import androidx.navigation.fragment.findNavController
 
 class HomeFragment : Fragment() {
 
@@ -27,6 +30,10 @@ class HomeFragment : Fragment() {
             val weight = weightEdit.text.toString().toDouble()
             val height = heightEdit.text.toString().toDouble()
 
+            val bmi = weight / (height * height)
+            //Toast.makeText(activity,bmi.toString(),Toast.LENGTH_LONG).show()
+            val bundle = bundleOf("bmi_score" to bmi)
+            findNavController().navigate(R.id.action_homeFragment_to_resultFragment,bundle)
         }
         return view
     }
--- app/src/main/java/com/nurhossain/bmicalculator/ResultFragment.kt
+++ app/src/main/java/com/nurhossain/bmicalculator/ResultFragment.kt
@@ -5,14 +5,22 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 
 class ResultFragment : Fragment() {
 
+    private lateinit var bmiScoreTv: TextView
+    private lateinit var categoryTv: TextView
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.fragment_result, container, false)
+
+        bmiScoreTv = view.findViewById(R.id.bmi_score_tv)
+        categoryTv = view.findViewById(R.id.category_tv)
+        val score = arguments?.getDouble("bmi_score")
+        bmiScoreTv.text = String.format("%.1f",score)
         return view
     }
 
